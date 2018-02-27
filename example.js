da12d39e3496a3d65abaf6a90a4184e03d615fbe
Brennan Cheung <git@brennancheung.com>
2018-02-27 15:25:37 -0800

    Example after

--- example.js
+++ example.js
@@ -1,16 +1,38 @@
+import { connect } from "react-redux";
 import React from 'react';
 
+const mapState = state => ({
+  username: state.username,
+  displayName: state.displayName,
+  hometown: state.hometown,
+  phoneNumber: state.phoneNumber
+});
+
 class MainApp extends React.Component {
+  handleConnectedChange = key => value => dispatch => {
+    dispatch({
+      type: 'SET_VALUE',
+      payload: {
+        key,
+        value
+      }
+    });
+  };
+
   render() {
     const {
-      someProp
+      someProp,
+      username,
+      displayName,
+      hometown,
+      phoneNumber
     } = this.props;
 
     return <div>
-      <input type="text" connect-to={username} />
-      <input type="text" connect-to={displayName} />
-      <input type="text" connect-to={hometown} />
-      <input type="text" connect-to={phoneNumber} />
+      <input type="text" value={username} onChange={this.handleConnectedChange("username")} />
+      <input type="text" value={displayName} onChange={this.handleConnectedChange("displayName")} />
+      <input type="text" value={hometown} onChange={this.handleConnectedChange("hometown")} />
+      <input type="text" value={phoneNumber} onChange={this.handleConnectedChange("phoneNumber")} />
       <button>Update</button>
     </div>;
   }
